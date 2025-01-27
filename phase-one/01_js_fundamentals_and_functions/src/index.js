4530207dd855f3f4f17319710acd619ae8c016bb
Emiley Palmquist <emiley.palmquist@gmail.com>
2025-01-27 10:34:07 -0800

    ADD: js fundamentals lecture starter code

---
+++ phase-one/01_js_fundamentals_and_functions/src/index.js
@@ -0,0 +1,51 @@
+//✅ Data Types 
+// 7 primitives (is not an object and has no methods or properties) 
+// Numbers, Strings, Booleans, Undefined, Null, Symbol, BigInt
+
+// Objects
+
+//✅ Variables
+// const - constant, can't be changed
+// let - can be changed
+// var - can be changed, has weird scoping, stick to let and const
+
+//✅ Equality Operators
+// === == != !==
+
+
+//✅ Comparison Operators
+// <, <=, > , >=
+
+
+//✅ Logical Operators
+// &&, ||, !, !!
+
+
+// + - / * ** (PEMDAS)
+// Paranthesis
+// Exponents
+// multiplication and division
+// addition and subtraction
+
+//✅ control flow
+// if ('') {
+//   console.log('truthy')
+// } else {
+//   console.log('falsey')
+// }
+
+
+// ✅ functions
+
+//✅ create function helloWorld() that console logs "hello, world!"
+
+//✅ create function addNums that calculates and returns the sum of two numbers
+
+
+//✅ Global Scope
+
+//✅ Function Scope
+
+//✅ Block Scope
+
+//✅ BONUS: higher-order functions
